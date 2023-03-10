acee8d9ebad938617c4031576d60fcd053d4ea02
blloyd132 <30760231+blloyd132@users.noreply.github.com>
2023-03-09 17:26:01 -0700

    Reworked CustomSkinset

--- ExampleObjects/ExampleSkinset.cs
+++ ExampleObjects/ExampleSkinset.cs
@@ -17,9 +17,9 @@ namespace FTKModLib.Example
             // All skinset items below can be set via a custom prefab, or with a skinset ID like:
             // Avatar = MakeAvatar(FTK_skinset.ID.blacksmith_Male); // This will set the male blacksmith as the avatar for this skinset (the avatar will be shared between the two classes)
             
-            Avatar = MakeAvatar(ExampleMod.assetBundleSkins.LoadAsset<GameObject>("Assets/player_Test.prefab"));
+            Avatar = MakeAvatar(ExampleMod.assetBundleSkins.LoadAsset<GameObject>("Assets/playerPaladin.prefab"));
             Armor = MakeArmor(ExampleMod.assetBundleSkins.LoadAsset<GameObject>("Assets/armorPaladin1.prefab"));
-            Boot = MakeBoots(ExampleMod.assetBundleSkins.LoadAsset<GameObject>("Assets/bootsPaladin1.prefab"));
+            Boot = MakeBoots(ExampleMod.assetBundleSkins.LoadAsset<GameObject>("Assets/bootsPaladin.prefab"));
             Helmet = MakeHelmet(ExampleMod.assetBundleSkins.LoadAsset<GameObject>("Assets/helmLeprechaun.prefab"));
             Backpack = MakeBackpack(ExampleMod.assetBundleSkins.LoadAsset<GameObject>("Assets/backpackLeprechaun.prefab"));
         }
